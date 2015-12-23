4f534de393f86db0c31bca109661f1597ee9b048
pearpages <pere@soms.cat>
2015-12-23 11:19:54 +0100

    inject own css js files with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ gulp.task('style', function() {
 
 gulp.task('inject', function() {
     var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
 
     var injectSrc = gulp.src(['./public/css/*.css','./public/js/*.js'], {read: false});
 
@@ -25,11 +26,12 @@ gulp.task('inject', function() {
 
     var options = {
         bowerJson: require('./bower.json'),
-        directory: './public/lib'
+        directory: './public/lib',
+        ignorePath: '../../public'
     };
 
     return gulp.src('./src/views/*.html')
-        .pipe(wiredep(options))
-        //.pipe(inject(injectSrc, injectOptions))
+        .pipe(wiredep(options)) //bower files
+        .pipe(inject(injectSrc, injectOptions)) //own files
         .pipe(gulp.dest('./src/views'));
 });
